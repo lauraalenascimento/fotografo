a3595dabc660d4f3456d8577b501ac5784dfc9e2
Laura Nascimento <110117512+lauraalenascimento@users.noreply.github.com>
2023-05-31 18:28:46 -0300

    Add files via upload

---
+++ Laura Alexandra/js/add.js
@@ -0,0 +1,11 @@
+const chavemodal = document.querySelector(".chavemodal");
+const btncancelar = document.querySelector(".btncancelar");
+const btnenviar = document.querySelector(".btnenviar");
+
+
+btnenviar.addEventListener('click',()=>{
+    if(btnenviar ===""){
+        alert("campo vazio")
+
+    }
+})
